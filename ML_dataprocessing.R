02fa18b07ef43b216bcaf4a5eb94216c34eff89f
FreedomX10A <63025678+FreedomX10A@users.noreply.github.com>
2020-04-01 20:58:02 -0400

    Update ML_dataprocessing.R

--- ML_dataprocessing.R
+++ ML_dataprocessing.R
@@ -1,18 +1,20 @@
 
 
 
+library(plyr)
+library(quantmod)
 
 
 
 obs_days = 60 
 future_days = 30
 
-df1 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (1).csv")
-df2 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (2).csv")
-df3 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (3).csv")
-df4 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (4).csv")
-df5 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (5).csv")
-df6 = read.csv(file = "./stockProject/data/ML_dataset/america_2020-03-27 (6).csv")
+df1 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (1).csv")
+df2 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (2).csv")
+df3 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (3).csv")
+df4 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (4).csv")
+df5 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (5).csv")
+df6 = read.csv(file = "./stockproject/data/ML_dataset/america_2020-03-27 (6).csv")
 
 df_merge = join(df1, df2, by="Ticker", type="inner")
 df_merge = join(df_merge, df3, by="Ticker", type="inner")
